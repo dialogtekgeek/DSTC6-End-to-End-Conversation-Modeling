bf9e800ce2c31ca0632c29dcf632df2aaebf531e
takaaki-hori <thori@merl.com>
2017-06-04 02:29:03 -0400

    Update make_trial_data.sh

--- tasks/twitter/make_trial_data.sh
+++ tasks/twitter/make_trial_data.sh
@@ -4,13 +4,13 @@
 stored_data=../../collect_twitter_dialogs/stored_data
 
 # extract train and dev sets
-echo extracting training data
+echo extracting training set
 ./extract_twitter_dialogs.py --data-dir $stored_data -t account_names_train.txt -o twitter_trial_data_train.txt
-echo extracting development data
+echo extracting development set
 ./extract_twitter_dialogs.py --data-dir $stored_data -t account_names_dev.txt -o twitter_trial_data_dev.txt
 
 # extract 500 samples from dev set randomly for evaluation
-echo extracting evaluation data
+echo extracting test set
 ./utils/sample_dialogs.py twitter_trial_data_dev.txt 500 > twitter_trial_data_eval.txt
 
 echo done
