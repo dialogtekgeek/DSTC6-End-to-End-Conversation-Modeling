bef5953545312e386dce39736136ad8caadd3c84
Takaaki Hori <thori@merl.com>
2017-09-23 22:52:20 -0400

    Add official test set extraction tools

---
+++ tasks/twitter/make_official_testset.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# you can change the directory path according to the location of stored data
+stored_data=../../collect_twitter_dialogs/official_stored_data
+
+# tweet ID list of the test set
+# (reference text is not provided, i.e., last turn of each dialog is empty)
+idlist=official_testset_ids.json
+gunzip -c ${idlist}.gz > $idlist
+
+echo extracting the official test set
+extract_official_twitter_testset.py \
+    --id-list $idlist \
+    --data-dir $stored_data \
+    --target official_account_names_test.txt \
+    --output twitter_official_data_test.txt
+
+echo done
